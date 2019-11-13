5b4137598b3d8fd1611e795972b9259ca949ee85
Schyler Troester <schyler@Schylers-MacBook-Air.local>
2019-11-13 13:26:02 -0600

    shoot logic

--- Models/JS/bullet.js
+++ Models/JS/bullet.js
@@ -1,7 +1,26 @@
-var Bullet = function(speed, xPos, yPos, dir) {
+var Bullet = function(speed, xPos, yPos, dir, width, height) {
   this.speed = speed;
-  //this.type = type;
   this.xPos = xPos;
   this.yPos = yPos;
   this.dir = dir;
+  this.width = width;
+  this.height = height;
+};
+Bullet.prototype.collide = function(otherobj) {
+  for (var i = 0; i < otherobj.length; i++){
+  var myleft = this.xPos;
+  var myright = this.xPos + this.width;
+  var mytop = this.yPos;
+  var mybottom = this.yPos + this.height;
+  var otherleft = otherobj[i].x;
+  var otherright = otherobj[i].x + otherobj[i].width;
+  var othertop = otherobj[i].y;
+  var otherbottom = otherobj[i].y + otherobj[i].height;
+
+  if (myleft >= otherleft && myright <= otherright && mytop >= othertop && mybottom <= otherbottom){
+    return true;
+  }
+  }
+  return false;
+
 };
